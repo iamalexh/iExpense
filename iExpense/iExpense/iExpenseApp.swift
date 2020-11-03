1bec6910d7c40b93d5859e778315b93bba9e4252
iamalexh <67149386+iamalexh@users.noreply.github.com>
2020-11-03 14:00:00 +0000

    Add files via upload

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Alex on 24/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
